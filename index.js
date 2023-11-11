a6932ccdfcc1bde5987929c98debce90c9534427
AUCB21 <aucontrerasb@gmail.com>
2023-11-11 20:15:09 -0300

    Added manifesto

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var textElement = document.querySelector(".historias h2");
+document.addEventListener("DOMContentLoaded", () => {
+  const textElement = document.querySelector(".historias h2");
 
   window.addEventListener("scroll", function () {
-    var rect = textElement.getBoundingClientRect();
-    var windowHeight =
+    const rect = textElement.getBoundingClientRect();
+    let windowHeight =
       window.innerHeight || document.documentElement.clientHeight;
-    var distanceToCenter = Math.abs(
+    let distanceToCenter = Math.abs(
       rect.top + rect.height / 2 - windowHeight / 2
     );
 
     // Definir una distancia máxima, puedes ajustar este valor según sea necesario
-    var maxDistance = windowHeight / 2;
+    let maxDistance = windowHeight / 1.2;
 
     // Calcular la opacidad en función de la distancia al centro
-    var opacity = 1 - Math.min(distanceToCenter / maxDistance, 1);
+    let opacity = 1 - Math.min(distanceToCenter / maxDistance, 1);
 
     textElement.style.opacity = opacity;
   });
+});
+
+const scrollToSection = (sectionId) => {
+  const section = document.getElementById(sectionId);
+
+  if (section) {
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: "smooth",
+    });
+  }
+};
+
+const queBuscamos = document.getElementById("purpouse");
+
+queBuscamos.addEventListener("click", () => {
+  scrollToSection("intro-text");
 });
